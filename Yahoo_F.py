7949190857230b2ee979c7d5a64d1cdda117e6f4
Chris Lattanzio <christopher.lattanzio@gmail.com>
2016-09-13 22:12:33 -0400

    Graph

--- Yahoo_F.py
+++ Yahoo_F.py
@@ -1,8 +1,27 @@
 #from pandas.io.data import DataReader
 from pandas_datareader import data
 from datetime import datetime
+import pandas as pd
+from pandas import Series
+import matplotlib.pyplot as plt
 
-aapl = data.DataReader('AAPL', 'yahoo', '1980-01-01')
-print(aapl.til())
+aapl = data.DataReader('AAPL', 'yahoo', '2015-01-01')
 
-aapl.to_csv('/Users/chrislattanzio/Python/Mkt_Data/aapl_data.csv')
+aapl['Moving Avg'] = Series.rolling(aapl['Adj Close'], 20).mean()
+
+#pd.rolling_mean(aapl['Adj Close'], 20)
+#print(aapl.tail())
+
+plot1 = pd.DataFrame(aapl, columns=['Adj Close','Moving Avg'])
+
+print(plot1.tail())
+
+plt.plot(plot1)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.title('Stock Closing Price')
+plt.show()
+
+
+
+#aapl.to_csv('/Users/chrislattanzio/Python/Mkt_Data/aapl_data.csv')
